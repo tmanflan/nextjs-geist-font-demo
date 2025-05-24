5b98853cf1db3676477cb4dbff5c168d6738e7f8
tmanflan <140356199+tmanflan@users.noreply.github.com>
2025-05-24 13:18:51 -0500

    Add next.config.js 1748110730604

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '/seed/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
