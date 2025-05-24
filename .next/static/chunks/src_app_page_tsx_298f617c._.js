49475fcb70db65e8f038a111da2e881bcf1067c4
tmanflan <140356199+tmanflan@users.noreply.github.com>
2025-05-24 13:19:06 -0500

    Add .next/static/chunks/src_app_page_tsx_298f617c._.js 1748110746015

---
+++ .next/static/chunks/src_app_page_tsx_298f617c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_44a7eaa2._.js",
+  "static/chunks/node_modules_1b095ab5._.js"
+],
+    source: "dynamic"
+});
